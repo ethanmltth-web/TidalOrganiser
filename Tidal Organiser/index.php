1f81c01825ae0253b3f0c14fd321ae566dc7a27d
ethanmltth-web <ethanmltth@gmail.com>
2026-05-06 14:56:13 +0800

    Delete Tidal Organiser directory

--- Tidal Organiser/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Landing page: show the introduction (index.html).
- * Sign-in is via the floating button; signed-in users can open app.php directly.
- */
-header('Content-Type: text/html; charset=utf-8');
-readfile(__DIR__ . '/index.html');
